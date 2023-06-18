1e932814d514d524c69aa9c455c4e70416fe6cae
Gerson <gersoncafilho@gmail.com>
2023-06-18 10:32:01 -0300

    resumo - clean code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from '@mui/material/Button';
 import ChatComponent from './ChatComponent';
 
 function App() {
+
+  const TEXT_HEADER = "Oi, sou um chatBot."
+
   return (
     <div className="App">
-      <ChatComponent />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello chat react <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="contained">Oi botão</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {TEXT_HEADER}
       </header>
+      <ChatComponent />
     </div>
   );
 }
